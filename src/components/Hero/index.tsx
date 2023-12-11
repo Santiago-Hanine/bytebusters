28229f852a9a0ba5a5093f4ff9eaee7850557a08
Santiago Hanine <santihanine@gmail.com>
2023-12-11 18:35:16 -0300

    Refactor Hero component styles

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -3,7 +3,7 @@ import Layout from '../Layout'
 
 const Hero = () => {
   return (
-    <Layout className='px-0 bg-hero-pattern h-[calc(100vh-5rem)] bg-cover bg-center bg-blend-lighten bg-[rgba(173,177,211,0.7)]'>
+    <Layout className='bg-hero-pattern h-[calc(100vh-5rem)] bg-cover bg-center bg-blend-lighten bg-[rgba(173,177,211,0.7)]'>
         <div className='h-full flex flex-col justify-end pb-16 lg:pb-44 xl:pb-52 gap-6 max-w-4xl'>
                 <h2 className='text-4xl font-medium lg:text-7xl'>Fix Your Computer Fast</h2>
                 <p className='text-lg font-light tracking-normal md:text-xl md:tracking-wide'>Reliable computer repair service in California. Get your computer back up and running in no time.</p>
