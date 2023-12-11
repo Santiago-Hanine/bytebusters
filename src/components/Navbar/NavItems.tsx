cb19b444ec4d914cfe612225b7d500727143dfec
Santiago Hanine <santihanine@gmail.com>
2023-12-11 18:18:59 -0300

    Add Navbar component and NavItems

---
+++ src/components/Navbar/NavItems.tsx
@@ -0,0 +1,22 @@
+
+    export const NavItems = () => {
+        const items = [
+            {
+                name: "Home",
+                href: "/"
+            },
+            {
+                name: "Services",
+                href: "/services"
+            },
+            {
+                name: "About",
+                href: "/about"
+            },
+            {
+                name: "Contact",
+                href: "/contact"
+            },
+        ];
+        return items;
+    }
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,25 @@
+
+import Layout from '../Layout'
+import { NavItems } from './NavItems'
+import Image from 'next/image'
+
+const Navbar = () => {
+  return (
+    <Layout className='bg-secondary sticky top-0 flex justify-between items-center h-20'>
+      <div className='flex items-center gap-2 text-xl'>
+        <Image src='/bytebusters-logo.png' width={50} height={50} alt='logo'/>
+        <h3>ByteBusters</h3>
+      </div>
+        <div className='hidden lg:flex gap-10'>
+            {NavItems().map((item, index) => (
+                <div className='' key={index}>
+                    <a href={item.href}>{item.name}</a>
+                </div>
+                )
+                )}
+        </div>
+    </Layout>
+  )
+}
+
+export default Navbar
