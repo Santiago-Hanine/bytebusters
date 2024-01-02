1e3a793145cab29562e7da5a0d52501acec8edfc
Santiago Hanine <santihanine@gmail.com>
2024-01-02 11:04:05 -0300

    Add CardContainer and Cards components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 
+import CardContainer from '@/components/CardContainer';
 import Hero from '@/components/Hero';
 import Text from '@/components/Text';
+import { cardData } from '@/components/Card/cardData';
+import Cards from '@/components/Cards';
 
 
 export default function Home() {
@@ -8,6 +11,9 @@ export default function Home() {
       <main>
         <Hero />
         <Text />
+        <CardContainer>
+          <Cards featuredCard={true} cards={cardData}/>
+        </CardContainer>
       </main>
   )
 }
