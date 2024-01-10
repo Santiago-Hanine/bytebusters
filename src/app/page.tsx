856bfd477b199df50b70cd26ad665bfe74097f33
Santiago Hanine <santihanine@gmail.com>
2024-01-10 18:21:12 -0300

    Add Bento, Testimonials, and Contact components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import Hero from '@/components/Hero';
 import Text from '@/components/Text';
 import { cardData } from '@/components/Card/cardData';
 import Cards from '@/components/Cards';
+import Bento from '@/components/Bento';
+import Testimonials from '@/components/Testimonials';
+import Contact from '@/components/Contact';
 
 
 export default function Home() {
@@ -14,6 +17,11 @@ export default function Home() {
         <CardContainer>
           <Cards featuredCard={true} cards={cardData}/>
         </CardContainer>
+        <Bento />
+        <Testimonials 
+          testimonial="ByteBusters provided exceptional computer repair service! Their team was prompt, knowledgeable, and fixed my laptop quickly. I'm grateful for their expertise and professionalism. Highly recommended!" 
+          name='Sophia'
+        />
       </main>
   )
 }
