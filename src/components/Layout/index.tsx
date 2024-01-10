d02907bd594277dd20525e745ded8cc08ee8eb13
Santiago Hanine <santihanine@gmail.com>
2024-01-10 18:21:44 -0300

    Add id prop to Layout component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,11 +1,12 @@
 import { clsx } from "clsx"
 
-const Layout = ({children, className} : {
+const Layout = ({children, className, id} : {
     children: React.ReactNode
     className?: string
+    id?:string
 }) => {
   return (
-    <div className={clsx("px-6 lg:px-24 xl:px-48 mx-auto w-full", className)}>
+    <div id={id} className={clsx("px-6 lg:px-24 xl:px-48 mx-auto w-full", className)}>
         {children}
     </div>
   )
